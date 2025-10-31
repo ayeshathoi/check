82e24c152106ab9be599d97f7bdb3e7da6d375c2
Ayesha Athoi <ayeshaathoi62@gmail.com>
2025-10-30 23:54:53 -0400

    up

--- backend.js
+++ backend.js
@@ -5,11 +5,10 @@ const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function saveScoreToSupabase(score1, score2) {
-  const randomId = Math.random().toString(36).substring(2, 10);
 
   const { data, error } = await supabase
     .from("scores")
-    .insert([{ id: randomId, score1, score2 }]);
+    .insert([{ score1, score2 }]);
 
   if (error) {
     console.error("Error saving score:", error);
@@ -17,3 +16,17 @@ export async function saveScoreToSupabase(score1, score2) {
     console.log("Score saved to Supabase:", data);
   }
 }
+
+
+// create table scores (
+//   id uuid default gen_random_uuid() primary key,
+//   score1 int not null,
+//   score2 int not null,
+//   created_at timestamp default now()
+// );
+
+// CREATE POLICY "Allow insert for anon" 
+// ON scores 
+// FOR INSERT 
+// TO anon 
+// WITH CHECK (true);
